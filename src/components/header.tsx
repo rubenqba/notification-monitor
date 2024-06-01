b69f82cf6d55d7eb68d94adf0609ccf4ddcbff03
Ruben Bresler <ruben.bresler@ext.cemex.com>
2024-06-01 01:17:52 +0000

    struggling with devcontainer in windows env

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4">
       <h1 className="text-xl font-bold">My Notification Dashboard</h1>
-      <p>Datos generales del servicio de notificaciones</p>
+      <p>1 Datos generales del servicio de notificaciones</p>
     </header>
   );
 };
